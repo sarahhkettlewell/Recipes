6d2f8cb9aac010d2387447bc2a3c8806cc71f855
arbdew <67021813+arbdew@users.noreply.github.com>
2020-08-10 18:29:42 +0100

    created recipe site with create react app and added himepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+
+<div className='homepage'>
+    <div className='directory-menu'>
+        <div className='recipe-item'>
+            <div className='recipe'>
+                <h1 className='title'>CURRY</h1>
+                <span className='subtitle'>RECIPES</span>
+            </div>
+        </div>
+        <div className='recipe-item'>
+            <div className='recipe'>
+                <h1 className='title'>MEAT</h1>
+                <span className='subtitle'>RECIPES</span>
+            </div>
+        </div>
+        <div className='recipe-item'>
+            <div className='recipe'>
+                <h1 className='title'>FISH</h1>
+                <span className='subtitle'>RECIPES</span>
+            </div>
+        </div>
+        <div className='recipe-item'>
+            <div className='recipe'>
+                <h1 className='title'>SIDES</h1>
+                <span className='subtitle'>RECIPES</span>
+            </div>
+        </div>
+        <div className='recipe-item'>
+            <div className='recipe'>
+                <h1 className='title'>DESSERT</h1>
+                <span className='subtitle'>RECIPES</span>
+            </div>
+        </div>
+    </div>
+</div>
+);
+
+export default HomePage;
