a5365522d095b49cb02b010b29594edcc38714e7
arbdew <67021813+arbdew@users.noreply.github.com>
2020-08-10 18:36:16 +0100

    changed title

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -14,7 +14,7 @@ const HomePage = () => (
         </div>
         <div className='recipe-item'>
             <div className='recipe'>
-                <h1 className='title'>MEAT</h1>
+                <h1 className='title'>MEAT DISHES</h1>
                 <span className='subtitle'>RECIPES</span>
             </div>
         </div>
