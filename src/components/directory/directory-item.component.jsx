2258969a45df82f8a474d90bb8c0bc0a3e714672
sarahhkettlewell <sarahhkettlewell@gmail.com>
2020-08-11 19:15:20 +0100

    added data, effects and basic functionality

---
+++ src/components/directory/directory-item.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import RecipeItem from '../recipe-item/recipe-item.component';
+
+import './directory-item.styles.scss';
+
+class Directory extends React.Component {
+    constructor () {
+        super();
+
+        this.state = {
+            sections: [{
+                title: 'curry dishes',
+                imageUrl:'https://i.imgur.com/vixekMk.jpg',
+                size: 'large',
+                id: 1
+            },
+            {
+                title: 'meat dishes',
+                imageUrl: 'https://i.imgur.com/F11C1e1.jpg?1',
+                size: 'large',
+                id: 2
+            },
+            {
+                title: 'fish dishes',
+                imageUrl: 'https://i.imgur.com/KHUGdjn.jpg',
+                size: 'large',
+                id: 3
+            },
+            {
+                title: 'side dishes',
+                imageUrl: 'https://i.imgur.com/D3CrNAi.jpg',
+                id: 4
+            },
+            {
+                title: 'desserts',
+                imageUrl: 'https://i.imgur.com/fImBltx.jpg?3',
+                id: 5
+            }]
+        }
+    }
+
+    render() {
+        return (
+           <div className='directory-menu'>
+               {this.state.sections.map(({ title, imageUrl, id, size}) => (
+                    <RecipeItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                   ))}
+           </div> 
+        );
+    }
+}
+
+export default Directory;
